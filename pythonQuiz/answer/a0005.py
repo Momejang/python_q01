82dde64da6d1b25d0b7ee6a1642aeb991bb06b63
momejang <aguila8484@gmail.com>
2022-05-01 17:09:24 +0900

    5번문제

---
+++ pythonQuiz/answer/a0005.py
@@ -0,0 +1,14 @@
+list2 = [(1,2),(3,4),(5,6)]
+
+for x in list2:
+    print('##########')
+    print(x)
+    for y in x:
+        print('$$$$$$$$$$$$$')
+        print(x)
+        print(y)
+        if y == 2 or y == 6:
+            # print(x)
+            # print(str(x[0]) + ' '+ str(x[1]))
+
+#
--- pythonQuiz/q0005.py
+++ pythonQuiz/q0005.py
@@ -1,12 +1,9 @@
-list2 = [(1,2),(3,4),(5,6)]
+list = [(1,2),(3,4),(5,6)]
 
-for x in list2:
-    print('##########')
-    print(x)
-    for y in x:
-        print('$$$$$$$$$$$$$')
-        print(x)
-        print(y)
-        if y == 2 or y == 6:
-            # print(x)
-            # print(str(x[0]) + ' '+ str(x[1]))
+for x in list:
+
+
+# for 문을 사용해서 숫자가 2 와 6이 사용된 내용을 출력하세요
+# 출력내용
+# (1, 2)
+# (5, 6)
