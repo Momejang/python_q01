5f172d999a7be05ba31bebc1e6b09b38193c653b
momejang <aguila8484@gmail.com>
2022-05-01 17:10:33 +0900

    5번문제 정답

--- pythonQuiz/answer/a0005.py
+++ pythonQuiz/answer/a0005.py
@@ -1,6 +1,10 @@
-list2 = [(1,2),(3,4),(5,6)]
+# for 문을 사용해서 숫자가 2 와 6이 사용된 내용을 출력하세요
+# 출력내용
+# (1, 2)
+# (5, 6)
+list = [(1,2),(3,4),(5,6)]
 
-for x in list2:
+for x in list:
     print('##########')
     print(x)
     for y in x:
@@ -8,7 +12,7 @@ for x in list2:
         print(x)
         print(y)
         if y == 2 or y == 6:
-            # print(x)
+            print(x)
             # print(str(x[0]) + ' '+ str(x[1]))
 
 #
