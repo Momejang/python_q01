c78a1b5a8b52c08dca6bb59cfc810968c2f90870
momejang <aguila8484@gmail.com>
2022-05-08 15:11:22 +0900

    2022.05.08 Quiz

---
+++ pythonQuiz/q0013.py
@@ -0,0 +1,15 @@
+# for문 을 활용할세요
+# 물자열 찾기 함수를 활용하세요
+# 아래와 같은 출력문을 완성하세요
+
+# python 에는 o 가 존재합니다
+# WELCOME 에는 해당문자가 존재하지 않습니다
+# Tutorial 에는 o 가 존재합니다
+# tistory 에는 o 가 존재합니다
+# github 에는 해당문자가 존재하지 않습니다
+
+
+stringArr = ['python','WELCOME','Tutorial','tistory','github']
+
+for x in stringArr :
+    #코드를 완성하세요
