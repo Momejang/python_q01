d6e9c3f87b555a212b7b8e9eafa6f5ac3bdf5656
momejang <aguila8484@gmail.com>
2022-05-15 19:03:34 +0900

    game ver 0.0.2

--- test_py/q0201_class.py
+++ test_py/q0201_class.py
@@ -3,13 +3,16 @@ class System:
     self.state = 'S'
   def help(self):
     print(f'사용가능 명령어')
-    print(f'1.리스트 출력')
+    print(f'1.헬프')
+    print(f'2.종료')
+    print(f'3.캐릭터생성')
+    print(f'4.캐릭터정보')
   def exit(self):
-    Self.stte = 'Q'
+    self.state = 'Q'
 
 class Character :
   name = ''
-  
+
   def __init__(self, name, lv):
     self.name = name
     self.lv = lv
@@ -22,18 +25,35 @@ class Character :
     else:
       print(f'레벨업에 실패하였습니다')
   def getInfo(self):
-    printf(f'{self.name} 의 레베은 {self.lv} 입니다')
+
+    print(f'{self.name} 의 레벨은 {self.lv} 입니다')
 
 # 시스템코드 생성
 system = System()
+# 캐릭터 저장공간
+chArr = []
 
 # 실행 명령어
 while (system.state != 'Q'):
-  print(f'명령을 입력해주세요')
-  cmd = input('명령')
-  print(f'입력한 명령 : {cmd}')
+    print(f'#############################################')
+    print(f'명령을 입력해주세요. -헬프 (명령어 리스트 출력)')
+    cmd = input('명령 :')
+    print(f'입력한 명령 : {cmd}')
 
-  if(cmd == '종료'):
-    system.exit()
-  if(cmd == '헬프'):
-    system.help()  
+    if(cmd == '종료'):
+        system.exit()
+
+    if(cmd == '헬프'):
+        system.help()
+
+    if(cmd == '캐릭터생성'):
+        name = input('캐릭터 이름 입력:')
+        lv = 1
+        chArr.append(Character(name, lv))
+
+    if(cmd == '캐릭터정보'):
+        if(len(chArr) < 1):
+            print(f'생성된 캐릭터가 없습니다')
+        else:
+            for x in chArr:
+                x.getInfo()
